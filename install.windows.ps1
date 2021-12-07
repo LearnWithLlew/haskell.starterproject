b09606622a10c55e02e254e3e1f6bb845a145383
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-12-07 01:16:17 +0000

    e updated powershell install
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- install.windows.ps1
+++ install.windows.ps1
@@ -1,16 +1,19 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
-#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.Python/master/install.windows.ps1 | Invoke-Expression
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/haskell.starterproject/master/install.windows.ps1 | Invoke-Expression
 
 # This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
 #
 
 
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+
 choco install haskell-dev
 
 # Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.Python.git C:\Code\ApprovalTests.Python
-cd C:\Code\ApprovalTests.Python
+& "C:\Program Files\Git\cmd\git.exe" clone https://raw.githubusercontent.com/LearnWithLlew/haskell.starterproject.get C:\Code\haskell.starterproject
+cd C:\Code\haskell.starterproject
+
+# Run starting haskell stuff
 cabal update
 cabal install --lib HUnit
 run_tests.bat
